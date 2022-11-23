8720552570393dfe6ca6841ace35f18f68c6b22b
jl <jl.walnutcreek@gmail.com>
2022-11-22 23:43:56 -0800

    add jumping

--- Godottestproject/scenes/player/testplayer.gd
+++ Godottestproject/scenes/player/testplayer.gd
@@ -6,6 +6,8 @@ var hkp = 0
 #The name of this variable stands for "Horizonal key press" which doesn't make much sense but it works for me. Also use this variable instead of checking if either the left or right key is pressed
 var friction = 0.8
 #lower the number the more friction there is.
+var Jdr = 15
+#this stands for jump duration. used to do stuff relating to variable jump height.
 
 
 
@@ -13,14 +15,32 @@ func _physics_process(delta):
 	#key press stuff
 	hkp = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	
+	#Left and right movment
 	if hkp == 1 and vsp.x < 150:
 		vsp.x = vsp.x +(60 - (60 * friction))
 	if hkp == -1 and vsp.x > -150:
 		vsp.x = vsp.x -(60 - (60 * friction))
 	if hkp == -0:
 		vsp.x = vsp.x *(friction) 
-	 #physics 
-	move_and_slide(vsp)
+	
+	#Jumping
+	if !Input.is_action_pressed("Jump"):
+		if is_on_floor(): 
+			Jdr = 15
+		else: 
+			Jdr = -1
+		
+	
+	if Input.is_action_pressed("Jump") and Jdr >= 0:
+		vsp.y = -180
+		Jdr = Jdr - 1
+	
+	
+	
+	#physics
+	vsp.y = vsp.y + 15 
+	vsp = move_and_slide(vsp,Vector2.UP)
+	
 	
 
 	
