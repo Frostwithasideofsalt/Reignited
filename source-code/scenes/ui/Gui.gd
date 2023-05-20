e08b0189d5f47680206a9ee1c87a0b485c747ee5
jl <jl.walnutcreek@gmail.com>
2023-05-19 22:06:34 -0700

    add health bar

--- source-code/scenes/ui/Gui.gd
+++ source-code/scenes/ui/Gui.gd
@@ -2,8 +2,10 @@ extends CanvasLayer
 
 func _process(delta):
 	visible =  not globallevel.paused
-	$Hcoin.text =  String(globallevel.hcoin) + "/12"
-	$Phealth.text =  String(globallevel.hp) + "/24"
+	$Bars1.scale = Vector2(globallevel.hp * -2 ,1)
+	$Bars2.scale = Vector2(globallevel.hcoin * -4 ,1)
+	
+	#test ui
 	$Scount.text = "Score; " + String(globallevel.score)
 	$Templabel.text =   "att energy; " +String(globallevel.nrg) + "/48 | fps: " + String(Engine.get_frames_per_second())# +  " | dsh energy; " +  String(globallevel.dsh)
 	
