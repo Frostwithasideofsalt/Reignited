50604c68824f8b88479f44e6a2c7bed3361967e7
jl <jl.walnutcreek@gmail.com>
2023-05-19 23:00:15 -0700

    pause clicks

--- source-code/scenes/ui/pause.gd
+++ source-code/scenes/ui/pause.gd
@@ -1,11 +1,17 @@
 extends Control
+var On = preload("res://sounds/gui/select-on.ogg")
+var Off = preload("res://sounds/gui/select-off.ogg")
 
 func _input(event):\
 	if Input.is_action_just_pressed("escape"):
 		globallevel.paused = not get_tree().paused
 		get_tree().paused = globallevel.paused
 		visible =  globallevel.paused
-		
+		if !globallevel.paused:
+			$on.play()
+		else:
+			$off.play()
+		$on.stop()
 
 
 
@@ -15,17 +21,19 @@ func _on_retry_pressed():
 	globallevel.paused = false
 	globallevel.invstate = 2
 	globallevel.hp = 0
+	$off.play()
 	visible =  globallevel.paused
 
 
 func _on_exit_pressed():
 	get_tree().paused = false
 	globallevel.paused = false
+	$off.play()
 	get_tree().change_scene("res://scenes/Main.tscn")
-	
 
 
 func _on_resume_pressed():
 	get_tree().paused = false
 	globallevel.paused = false
 	visible =  globallevel.paused
+	$off.play()
