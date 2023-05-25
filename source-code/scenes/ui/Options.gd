a6f5c34734f6da29cd516bf68759746e52d665da
jl <jl.walnutcreek@gmail.com>
2023-05-24 21:58:00 -0700

    add more to options and more keybinds

--- source-code/scenes/ui/Options.gd
+++ source-code/scenes/ui/Options.gd
@@ -4,6 +4,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("escape"):
 		get_tree().change_scene("res://scenes/Main.tscn")
 	$Fps.text = "Show FPS" + " (" + String(globalsetting.SFPS) + ")"
+	$Exit.text = "Exit" + " (" + String(Input.is_action_pressed("key")) + ")"
 
 func _on_Exit_pressed():
 	get_tree().change_scene("res://scenes/Main.tscn")
@@ -15,3 +16,7 @@ func _on_Mute_pressed():
 
 func _on_Fps_pressed():
 	globalsetting.SFPS = not(globalsetting.SFPS)
+
+
+func _on_fullscreen_pressed():
+	 OS.window_fullscreen = !OS.window_fullscreen
