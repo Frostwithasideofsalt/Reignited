f4a8bc8599ce07ddd25980aeac6f15c9645aa58a
jl <jl.walnutcreek@gmail.com>
2023-11-04 23:41:14 -0700

    done for today

--- scenes/objects/main/collected-hcoin.gd
+++ scenes/objects/main/collected-hcoin.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite
 var phase = 1
 var vsp = -3
+
+
 func _physics_process(delta):
 	vsp = vsp + 0.25
 	match(phase): 
@@ -9,6 +11,7 @@ func _physics_process(delta):
 			if vsp >= 3:
 				play("shine")
 				set_frame(0)
+				$AudioStreamPlayer2D.playing = true
 				phase = 2
 				rotate((randi() % 100)/2)
 		2: 
