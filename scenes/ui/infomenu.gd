c6b2a7c613df08be34fba67c976321f50daa7619
jl <jl.walnutcreek@gmail.com>
2023-11-05 21:46:17 -0800

    add links to info

--- scenes/ui/infomenu.gd
+++ scenes/ui/infomenu.gd
@@ -2,3 +2,11 @@ extends HBoxContainer
 
 func _on_Quit_pressed():
 	get_tree().change_scene("res://scenes/Main.tscn")
+
+
+func _on_Github_pressed():
+	OS.shell_open("https://github.com/Frostwithasideofsalt/Reignited")
+
+
+func _on_Soundcloud_pressed():
+	OS.shell_open("https://soundcloud.com/rodiroddude/sets/flames-land-re-ignited")
