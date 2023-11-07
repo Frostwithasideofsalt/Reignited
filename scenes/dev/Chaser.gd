01f343e42d464fe0882dc4f8488f4f68abd0bb80
jl <jl.walnutcreek@gmail.com>
2023-11-06 20:04:29 -0800

    more mr beast mode stuff i guess??? lmao

--- scenes/dev/Chaser.gd
+++ scenes/dev/Chaser.gd
@@ -5,22 +5,32 @@ var b = 0
 var c = 0
 
 var s = 3.5
+var active = false
 
 # Called when the node enters the scene tree for the first time.
+
 func _physics_process(delta):
-	if not((position.x == globallevel.playerpos.x) or (position.y == globallevel.playerpos.y)):
-		a =(abs(position.x - globallevel.playerpos.x)) /((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y)))
-		b = (position.x - globallevel.playerpos.x) / (abs(position.x - globallevel.playerpos.x)) * -1
-		c = (position.y - globallevel.playerpos.y) / (abs(position.y - globallevel.playerpos.y)) * -1
-	position.x = position.x + (a*b)* s
-	position.y = position.y + ((1 - a) * c)* s
+	
+	if Input.is_action_just_pressed("debug") and 500 > ((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y))):
+		visible = true
+		active = true
+		globallevel.score = 3000
+		
+	if active == true:
+		if not((position.x == globallevel.playerpos.x) or (position.y == globallevel.playerpos.y)):
+			a =(abs(position.x - globallevel.playerpos.x)) /((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y)))
+			b = (position.x - globallevel.playerpos.x) / (abs(position.x - globallevel.playerpos.x)) * -1
+			c = (position.y - globallevel.playerpos.y) / (abs(position.y - globallevel.playerpos.y)) * -1
+		position.x = position.x + (a*b)* s
+		position.y = position.y + ((1 - a) * c)* s
 	
 
-	if 30 > ((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y))):
-		globallevel.hp = -0
-		globallevel.invstate = 60
-	elif 240 < ((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y))):
-		s = 9
-	else:
-		s = 3.5
-	print (s)
+		if 30 > ((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y))):
+			globallevel.hp = -0
+			globallevel.invstate = 60
+			queue_free()
+			globallevel.score = 0
+		elif 240 < ((abs(position.x - globallevel.playerpos.x)) + (abs(position.y - globallevel.playerpos.y))):
+			s = 9
+		else:
+			s = 3.5
