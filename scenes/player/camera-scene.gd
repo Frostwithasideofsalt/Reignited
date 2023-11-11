2a0c91ab26e37b2003b5beffc2c4632078faefc0
jl <jl.walnutcreek@gmail.com>
2023-11-10 20:29:29 -0800

    fix minor mistake

--- scenes/player/camera-scene.gd
+++ scenes/player/camera-scene.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Node2D
 func _physics_process(_delta):
 	position.y  = position.y + ((globallevel.camseek.y) - (position.y) / 32)
 	position.x  = position.x + ((globallevel.camseek.x) - (position.x) / 32)
