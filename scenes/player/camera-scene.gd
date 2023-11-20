0fa39712ece175e906c4df83405e63b45d9a0faf
jl <jl.walnutcreek@gmail.com>
2023-11-20 00:31:46 -0800

    whar

--- scenes/player/camera-scene.gd
+++ scenes/player/camera-scene.gd
@@ -2,3 +2,10 @@ extends Node2D
 func _physics_process(_delta):
 	position.y  = position.y + ((globallevel.camseek.y) - (position.y) / 32)
 	position.x  = position.x + ((globallevel.camseek.x) - (position.x) / 32)
+	
+#	if abs(globallevel.camseek.x) <= 0.00002:
+#		position.x = 0
+#		globallevel.camseek.x = 0
+#	if abs(globallevel.camseek.y) <= 0.00002:
+#		position.y = 0
+#		globallevel.camseek.y = 0
